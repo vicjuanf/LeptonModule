e2015e007257b0f842b06cce2db9050046d2c60a
vicjuanf <vicjuanf@gmail.com>
2017-05-03 13:37:21 +0200

    Update raspberrypi_capture.c

--- software/raspberrypi_capture/raspberrypi_capture.c
+++ software/raspberrypi_capture/raspberrypi_capture.c
@@ -35,7 +35,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <linux/spi/spidev.h>
 #include <limits.h>
 #include <math.h>
-#include <opencv.h>
+#include <cv.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
